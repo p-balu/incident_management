1ae2962dbe50cbcfc5cf1fc57d63c756a724f7ae
Balachanderrao <palepubalachanderrao25@gmail.com>
2021-04-10 06:23:16 +0530

    big fixes

--- config/app.js
+++ config/app.js
@@ -49,6 +49,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(function (req, res, next) {
   next(createError(404));
 });
+console.log("env", process.env.NODE_ENV);
 
 // error handler
 app.use(function (err, req, res, next) {
