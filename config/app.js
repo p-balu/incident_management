7c2faf04480393de71903e300566ce8a9ec0dba0
Balachanderrao <palepubalachanderrao25@gmail.com>
2021-04-10 06:27:49 +0530

    changes

--- config/app.js
+++ config/app.js
@@ -45,6 +45,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
+console.log("enivronment", process.env.NODE_ENV);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
